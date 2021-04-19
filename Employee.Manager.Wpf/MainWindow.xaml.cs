dcf3147c0cd113ae066873be0e2f59d18939d88c
Andrey Andreev <andrey.andreev@quajoo.com>
2021-04-19 18:08:55 +0200

    init

---
+++ Employee.Manager.Wpf/MainWindow.xaml.cs
@@ -0,0 +1,27 @@
+﻿using EmployeeManager.DataAccess;
+using EmployeeManager.ViewModel;
+using System.Windows;
+
+namespace EmployeeManager.Wpf
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private MainViewModel _viewModel;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            _viewModel = new MainViewModel(new EmployeeDataProvider());
+            DataContext = _viewModel;
+            Loaded += MainWindow_Loaded;
+        }
+
+        private void MainWindow_Loaded(object sender, RoutedEventArgs e)
+        {
+            _viewModel.Load();
+        }
+    }
+}
---
+++ EmployeeManager.ViewModel/EmployeeViewModel.cs
@@ -0,0 +1,92 @@
+﻿using EmployeeManager.Common.DataProvider;
+using EmployeeManager.Common.Model;
+using EmployeeManager.ViewModel.Command;
+using System;
+
+namespace EmployeeManager.ViewModel
+{
+    public class EmployeeViewModel : ViewModelBase
+    {
+        private readonly Employee _employee;
+        public IEmployeeDataProvider _employeeDataProvider { get; }
+
+        public EmployeeViewModel(Employee employee, IEmployeeDataProvider employeeDataProvider)
+        {
+            _employee = employee;
+            _employeeDataProvider = employeeDataProvider;
+            SaveCommand = new DelegateCommand(Save, () => CanSave);
+        }
+        public DelegateCommand SaveCommand { get; }
+
+        public string FirstName
+        {
+            get => _employee.FirstName;
+            set
+            {
+                if (_employee.FirstName != value)
+                {
+                    _employee.FirstName = value;
+                    RaisePropertyCahnged();
+                    RaisePropertyCahnged(nameof(CanSave));
+                    SaveCommand.RaiseCanExecuteChanged();
+                }
+            }
+        }
+        public DateTimeOffset EntryDate
+        {
+            get => _employee.EntryDate;
+            set
+            {
+                if (_employee.EntryDate != value)
+                {
+                    _employee.EntryDate = value;
+                    RaisePropertyCahnged();
+                }
+            }
+        }
+        public DateTime EntryDateTime
+        {
+            get => _employee.EntryDate.DateTime;
+            set
+            {
+                if (_employee.EntryDate != value)
+                {
+                    _employee.EntryDate = value;
+                    RaisePropertyCahnged();
+                }
+            }
+        }
+
+        public int JobRoleId
+        {
+            get => _employee.JobRoleId;
+            set
+            {
+                if (_employee.JobRoleId != value)
+                {
+                    _employee.JobRoleId = value;
+                    RaisePropertyCahnged();
+                }
+            }
+        }
+
+        public bool IsCoffeeDrinker
+        {
+            get => _employee.IsCoffeeDrinker;
+            set
+            {
+                if (_employee.IsCoffeeDrinker != value)
+                {
+                    _employee.IsCoffeeDrinker = value;
+                    RaisePropertyCahnged();
+                }
+            }
+        }
+
+        public bool CanSave => !string.IsNullOrEmpty(FirstName);
+        public void Save()
+        {
+            _employeeDataProvider.SaveEmployee(_employee);
+        }
+    }
+}
